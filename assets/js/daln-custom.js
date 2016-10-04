e9e01fcb6a72288eb620c871de17c174af748f1b
Wasfi Momen <wasfimomenwim@gmail.com>
2016-10-04 18:42:31 -0400

    Refactored getJson to Ajax, data working, next step is adding the config

--- assets/js/daln-custom.js
+++ assets/js/daln-custom.js
@@ -6,6 +6,87 @@ jQuery(document).ready(function($) {
     $().UItoTop({ easingType: 'easeOutQuart'});
 
 
+    /**************************************************
+     * Index.html:                                    *
+     *  - AJAX call to get data and populate on page. *
+     **************************************************/
+
+
+    // $.getJSON( "data.json", function( data ) {
+    // //   console.log(data); // List the data
+    //   var size = Object.keys(data).length; // amount of Objects in the data. MAY NOT WORK IN IE.
+    //   var items = [];
+    //
+    //
+    //   // get all the titles and descriptions of the posts.
+    //   for(var i=0; i <= 29; i++) {
+    //
+    //       var listtitle = data[i].title;
+    //       var listdesc = data[i].description;
+    //
+    //       if( typeof(listtitle) != 'undefined' || typeof(listdesc) != 'undefined') {
+    //     //   console.log(listtitle + " | " + listdesc);
+    //       items.push( "<li class= 'span3 item-block'> <div class='desc'><a href='#'>" + listtitle + "</a> <p> <em>" + listdesc +"</em> </p> </div> </li>" );
+    //   } else if (typeof(listtitle) == 'undefined') {
+    //     //   console.log("No title | " + listdesc);
+    //     items.push( "<li class= 'span3 item-block'> <div class='desc'><a href='#'> No Title </a> <p> <em>" + listdesc +"</em> </p> </div> </li>" );
+    // } else {
+    //         // console.log(listtitle + "| No description.");
+    //         items.push( "<li class= 'span3 item-block'> <div class='desc'><a href='#'>" + listtitle + "</a> <p> <em> No description </em> </p> </div> </li>" );
+    //     }
+    //     // console.log(items);
+    //
+    //   }
+    //
+    // for(var j=0; j <= items.length; j++) {
+    //     $(".thumbnails").append(items[j]);
+    // }
+    //
+    // });
+
+    $.ajax({
+        url: "data.json",
+        data: { format: "json"},
+        error: function () {
+            console.log("Nah man.");
+        },
+        success: function( data ) {
+            //   console.log(data); // List the data
+            var size = Object.keys(data).length; // amount of Objects in the data. MAY NOT WORK IN IE.
+            var items = [];
+
+
+              // get all the titles and descriptions of the posts.
+              for(var i=0; i <= 29; i++) {
+
+                  var listtitle = data[i].title;
+                  var listdesc = data[i].description;
+
+                  if( typeof(listtitle) != 'undefined' || typeof(listdesc) != 'undefined') {
+                //   console.log(listtitle + " | " + listdesc);
+                  items.push( "<li class= 'span3 item-block'> <div class='desc'><a href='#'>" + listtitle + "</a> <p> <em>" + listdesc +"</em> </p> </div> </li>" );
+                } else if (typeof(listtitle) == 'undefined') {
+                //   console.log("No title | " + listdesc);
+                items.push( "<li class= 'span3 item-block'> <div class='desc'><a href='#'> No Title </a> <p> <em>" + listdesc +"</em> </p> </div> </li>" );
+                } else {
+                    // console.log(listtitle + "| No description.");
+                    items.push( "<li class= 'span3 item-block'> <div class='desc'><a href='#'>" + listtitle + "</a> <p> <em> No description </em> </p> </div> </li>" );
+                }
+                // console.log(items);
+
+              }
+
+                for(var j=0; j <= items.length; j++) {
+                    $(".thumbnails").append(items[j]);
+                }
+
+                console.log("Posts loaded successfully.");
+            },
+            type:"GET"
+        });
+
+
+
     // Detail Page
 
     // Added collapse function for dropdown
@@ -49,6 +130,8 @@ jQuery(document).ready(function($) {
 
         console.log(assetVid);
     });
+
+
 
 
 });
