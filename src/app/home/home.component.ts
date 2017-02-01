616eff2095c40d13a3fdbcd9559f1cfbc4429275
Wasfi Momen <wasfimomenwim@gmail.com>
2017-02-01 10:51:35 -0500

    Loads of CSS changes

--- src/app/home/home.component.ts
+++ src/app/home/home.component.ts
@@ -4,14 +4,7 @@ import { Post } from '../model/post-model';
 
 @Component({
   selector: 'home',
-  template: `
-    <div class=" container">
-         <post-list [postList]="posts"></post-list>
-    </div>
-
-    <daln-footer></daln-footer>
-
-`,
+  templateUrl: './home.component.html',
   styleUrls: ['./home.component.css']
 })
 export class HomeComponent implements OnInit {
