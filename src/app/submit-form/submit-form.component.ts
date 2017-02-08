70345a4f642b05decc3de32685fd09f4637672f9
Wasfi Momen <wasfimomenwim@gmail.com>
2017-02-08 12:11:10 -0500

    submit form beginning

--- src/app/submit-form/submit-form.component.ts
+++ src/app/submit-form/submit-form.component.ts
@@ -1,15 +1,27 @@
 import { Component, OnInit } from '@angular/core';
+import { FormBuilder, FormGroup, Validators } from '@angular/forms';
+
 
 @Component({
   selector: 'app-submit-form',
   templateUrl: './submit-form.component.html',
   styleUrls: ['./submit-form.component.css']
 })
-export class SubmitFormComponent implements OnInit {
+export class SubmitFormComponent {
 
-  constructor() { }
+    // user: FormGroup;
+    //  constructor(private fb: FormBuilder) {}
+    //  ngOnInit() {
+    //    this.user = this.fb.group({
+    //      name: ['', [Validators.required, Validators.minLength(2)]],
+    //      account: this.fb.group({
+    //        email: ['', Validators.required],
+    //        confirm: ['', Validators.required]
+    //      })
+    //    });
+    //  }
 
-  ngOnInit() {
-  }
-
+    //  onSubmit({ value, valid }: { value: User, valid: boolean }) {
+    //    console.log(value, valid);
+    //  }
 }
