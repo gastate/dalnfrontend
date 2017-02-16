34167913b8e308ebd36661e3d79f56f698a6f457
Wasfi Momen <wasfimomenwim@gmail.com>
2017-02-16 15:55:32 -0500

    testing json response of new endpoint

---
+++ dist/out-tsc/app/services/search.service.js
@@ -0,0 +1,38 @@
+var __decorate = (this && this.__decorate) || function (decorators, target, key, desc) {
+    var c = arguments.length, r = c < 3 ? target : desc === null ? desc = Object.getOwnPropertyDescriptor(target, key) : desc, d;
+    if (typeof Reflect === "object" && typeof Reflect.decorate === "function") r = Reflect.decorate(decorators, target, key, desc);
+    else for (var i = decorators.length - 1; i >= 0; i--) if (d = decorators[i]) r = (c < 3 ? d(r) : c > 3 ? d(target, key, r) : d(target, key)) || r;
+    return c > 3 && r && Object.defineProperty(target, key, r), r;
+};
+var __metadata = (this && this.__metadata) || function (k, v) {
+    if (typeof Reflect === "object" && typeof Reflect.metadata === "function") return Reflect.metadata(k, v);
+};
+import { Injectable } from '@angular/core';
+import { Jsonp, Http } from '@angular/http';
+import { Observable } from 'rxjs/Rx';
+import 'rxjs/add/operator/map';
+import 'rxjs/add/operator/catch';
+import 'rxjs/add/operator/toPromise';
+import { environment } from '../../environments/environment.prod';
+var SearchService = (function () {
+    function SearchService(_http, _jsonp) {
+        this._http = _http;
+        this._jsonp = _jsonp;
+        this.endPoint = environment.API_ENDPOINTS;
+    }
+    SearchService.prototype.search = function (term) {
+        console.log(this.endPoint.search_posts + "/" + term);
+        return this._jsonp.get(this.endPoint.search_posts + "/" + term).map(function (res) {
+            var posts = res.json();
+            console.log("Get All Posts ", posts);
+            return posts;
+        }).catch(function (error) { return Observable.throw(error.json().error || 'Server error'); });
+    };
+    return SearchService;
+}());
+SearchService = __decorate([
+    Injectable(),
+    __metadata("design:paramtypes", [Http, Jsonp])
+], SearchService);
+export { SearchService };
+//# sourceMappingURL=../../../../src/app/services/search.service.js.map
---
+++ dist/out-tsc/environments/environment.prod.js
@@ -0,0 +1,14 @@
+var api_url = "https://tg1vruzadg.execute-api.us-west-1.amazonaws.com/production";
+var all_posts = api_url + "/posts/all";
+var post = api_url + "/posts/get";
+var search_posts = api_url + "/posts/search";
+export var environment = {
+    production: true,
+    API_ENDPOINTS: {
+        api_url: api_url,
+        all_posts: all_posts,
+        post: post,
+        search_posts: search_posts
+    }
+};
+//# sourceMappingURL=../../../src/environments/environment.prod.js.map
