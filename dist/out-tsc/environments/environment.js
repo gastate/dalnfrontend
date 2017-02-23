25763797e8c2c9b69d408e0686102b6faafa2916
Wasfi Momen <wasfimomenwim@gmail.com>
2017-02-23 15:37:48 -0500

    Testing href

--- dist/out-tsc/environments/environment.js
+++ dist/out-tsc/environments/environment.js
@@ -1,14 +1,22 @@
-var api_url = "https://tg1vruzadg.execute-api.us-west-1.amazonaws.com/production";
+var api_url = "http://ec2-54-211-221-216.compute-1.amazonaws.com:8080/dalnws/api/DALNService";
 var all_posts = api_url + "/posts/all";
-var post = api_url + "/posts/get/";
-var search_posts = api_url + "/posts/search/";
+var create_post = api_url + "/posts/create";
+var page_posts = api_url + "/posts/";
+var post = api_url + "/posts/";
+var search_posts = api_url + "/posts/search=";
+var upload_media = api_url + "/upload";
+var update_post = api_url + "/update";
 export var environment = {
-    production: true,
+    production: false,
     API_ENDPOINTS: {
-        api_url: api_url,
         all_posts: all_posts,
+        api_url: api_url,
+        create_post: create_post,
+        page_posts: page_posts,
         post: post,
-        search_posts: search_posts
+        search_posts: search_posts,
+        upload_media: upload_media,
+        update_post: update_post
     }
 };
 //# sourceMappingURL=../../../src/environments/environment.js.map
