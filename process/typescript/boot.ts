249536f77251da57876fc9c05dfde8c832e27ea2
Wasfi Momen <wasfimomenwim@gmail.com>
2017-02-28 16:27:54 -0500

    returned package.json to previous release of angular-cli

--- process/typescript/boot.ts
+++
@@ -1,9 +0,0 @@
-
-
-import {bootstrap} from 'angular2/platform/browser';
-import {HTTP_PROVIDERS} from 'angular2/http';
-import {ROUTER_PROVIDERS} from 'angular2/router';
-import {AppComponent} from './app.component';
-import {PostService} from './PostService';
-
-bootstrap(AppComponent, [HTTP_PROVIDERS, ROUTER_PROVIDERS, PostService]);
