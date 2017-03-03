69cbe59a05255d5c1e78286d73defdb608e82269
Wasfi Momen <wasfimomenwim@gmail.com>
2017-03-03 01:29:24 -0500

    Completed tweet and facebook widgets, but router change stops update

--- src/app/app-footer/app-footer.component.ts
+++ src/app/app-footer/app-footer.component.ts
@@ -1,5 +1,5 @@
 import {Component, OnInit} from '@angular/core';
-import {Router} from '@angular/router';
+import {Router, NavigationEnd, ActivatedRoute } from '@angular/router';
 
 @Component({
   selector: 'daln-footer',
@@ -8,25 +8,56 @@ import {Router} from '@angular/router';
 })
 export class AppFooterComponent implements OnInit {
 
-  constructor(private _router: Router) {
-  }
+  constructor(private _router: Router,
+              private _activatedRoute : ActivatedRoute
+    ) {}
 
-  ngOnInit() {
-  }
+    ngOnInit() {
+      this._router.events
+        .filter(event => event instanceof NavigationEnd)
+        .map(() => this._activatedRoute)
+        .map(route => {
+          while (route.firstChild) route = route.firstChild;
+          return route;
+        })
+        .filter(route => route.outlet === 'primary')
+        .mergeMap(route => route.data)
+        .subscribe((event) => {this.twitterView(); this.faceBookView();});
+    }
 
-  // ngAfterViewInit () {
-  //          !function(d,s,id){
-  //              var js: any,
-  //                  fjs=d.getElementsByTagName(s)[0],
-  //                  p='https';
-  //              if(!d.getElementById(id)){
-  //                  js=d.createElement(s);
-  //                  js.id=id;
-  //                  js.src=p+"://platform.twitter.com/widgets.js";
-  //                  fjs.parentNode.insertBefore(js,fjs);
-  //              }
-  //          }
-  //          (document,"script","twitter-wjs");
-  //  }
+
+  ngAfterViewInit () {
+
+
+   }
+
+   twitterView() : void {
+
+       !function(d,s,id){
+           var js: any,
+               fjs=d.getElementsByTagName(s)[0],
+               p='https';
+           if(!d.getElementById(id)){
+               js=d.createElement(s);
+               js.id=id;
+               js.src=p+"://platform.twitter.com/widgets.js";
+               fjs.parentNode.insertBefore(js,fjs);
+           }
+       }
+       (document,"script","twitter-wjs");
+
+   }
+
+   faceBookView() : void {
+
+        (function(d, s, id) {
+          var js, fjs = d.getElementsByTagName(s)[0];
+          if (d.getElementById(id)) return;
+          js = d.createElement(s); js.id = id;
+          js.src = "//connect.facebook.net/en_US/sdk.js#xfbml=1&version=v2.8";
+          fjs.parentNode.insertBefore(js, fjs);
+        }(document, 'script', 'facebook-jssdk'));
+
+   }
 
 }
