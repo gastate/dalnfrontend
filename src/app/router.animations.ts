657193286a2085a214f1b1aa1455abfa6289489b
Wasfi Momen <wasfimomenwim@gmail.com>
2017-03-31 10:53:43 -0400

    Backup Files added

--- src/app/router.animations.ts
+++
@@ -1,9 +0,0 @@
-import {trigger, state, animate, style, transition} from '@angular/core';
-
-export function routerTransition() {
-    return slideToLeft();
-}
-
-function slideToLeft() {
-    
-}
--- src/app/state/app-store.ts
+++
@@ -1,7 +0,0 @@
-import { RightsProfile } from './rights';
-import { DescriptionProfile } from './description';
-
-export interface AppStore {
-  rights: RightsProfile,
-  description: DescriptionProfile
-}
