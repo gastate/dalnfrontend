f83a910b4ffa3577688bdd0bc71cc8462412e157
Wasfi Momen <wasfimomenwim@gmail.com>
2017-06-16 12:52:56 -0400

    added correct location for spec.ts in player

--- src/app/shared/player/player.component.spec.ts
+++ src/app/shared/player/player.component.spec.ts
@@ -3,7 +3,7 @@ import { async, ComponentFixture, TestBed } from '@angular/core/testing';
 import { By } from '@angular/platform-browser';
 import { DebugElement } from '@angular/core';
 
-import { PlayerComponentComponent } from './player-component.component';
+import { PlayerComponentComponent } from './player.component';
 
 describe('PlayerComponentComponent', () => {
   let component: PlayerComponentComponent;
