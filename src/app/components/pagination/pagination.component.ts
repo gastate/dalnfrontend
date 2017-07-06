b69cfae6ac6543df3d6e59e34b5afa94305544b9
CodeFluent <wasfimomenwim@gmail.com>
2017-07-06 12:36:24 -0400

    got buttons up

--- src/app/components/pagination/pagination.component.ts
+++ src/app/components/pagination/pagination.component.ts
@@ -26,6 +26,8 @@ export class PaginationComponent implements OnInit, OnChanges {
 
 
   resultsPerPage: number;
+  buttonArray: number[] = [];
+
 
 
   constructor(_searchService: SearchService) {
@@ -34,30 +36,28 @@ export class PaginationComponent implements OnInit, OnChanges {
 
   ngOnInit() {
      this.resultsPerPage = this.searchService.resultsSize;
-     console.log("pagination startOffset", this.startOffset);
   }
 
   getPagedPost() {
       let lastIndex = (this.startOffset * this.resultsPerPage - 1);
       let firstIndex = (lastIndex - this.resultsPerPage + 1);
       this.calculateButtonRange();
-      console.log("pagination resultList", this.resultList);
   }
 
   calculateButtonRange() {
       let numberOfButtons = this.resultList.length / this.resultsPerPage;
-      let buttonArray: number[] = [];
 
       for(var i = 0; i < numberOfButtons; i++) {
-          buttonArray.push(this.startOffset);
+          this.buttonArray.push(this.startOffset);
           this.startOffset++;
       }
-      console.log("Button Array", buttonArray);
+      console.log("Button Array", this.buttonArray);
   }
 
   ngOnChanges(changes: SimpleChanges) {
       if(changes['resultList']) {
           console.log("pagination change", this.resultList);
+          this.getPagedPost();
       }
   }
 
