069580c0c72fe719aa434f9c087c740c7fd56aef
Wasfi Momen <wasfimomenwim@gmail.com>
2017-08-19 18:04:56 -0400

    reverting location strat since s3

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -110,7 +110,7 @@ import { SafePipe } from './safe.pipe';
     AppRoutingModule,
     NgbModule.forRoot()
   ],
-  providers: [AuthService, CognitoUtil, PostService, SearchService, UserLoginService, DynamoDBService],
+  providers: [AuthService, CognitoUtil, PostService, SearchService, UserLoginService, DynamoDBService, {provide: LocationStrategy, useClass: HashLocationStrategy}],
   bootstrap: [AppComponent]
 })
 export class AppModule {
