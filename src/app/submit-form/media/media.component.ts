6fd0b81e34b0b4bff4855212f18e63e7456805b2
Polyergic <me@shadsterling.com>
2017-08-29 08:51:38 -0400

    Use new endpoint to get presigned link

--- src/app/submit-form/media/media.component.ts
+++ src/app/submit-form/media/media.component.ts
@@ -18,16 +18,12 @@ export class MediaComponent implements OnInit {
 
 
     submitService: SubmitFormService;
-    // fileList: FileList;
     fileInfos: any[] = [];
     fileBad:number = 0;
     fileDone:number = 0;
 
-    // fileName: string;
     errorMessage: string;
     succeedMessage: string;
-    // suggestMessage: string;
-    // percentUploaded: number;
 
     nextEnabled:boolean = true;
 
@@ -49,11 +45,7 @@ export class MediaComponent implements OnInit {
     setMedia(event) {
         let fn: string = this.constructor.name + "#uploadFiles";  // tslint:disable-line:no-unused-variable
         console.log( fn + ": invoked" );
-        // this.fileList = event.target.files;
-        // console.log(this.fileList);
 
-        // for (var i = 0; i < this.fileList.length; i++) {
-        //     let file = this.fileList[ i ];
         if( event.target.files.length > 0 ) {
             for( let file of event.target.files ) {
                 console.log( fn+": file ", file );
@@ -65,8 +57,6 @@ export class MediaComponent implements OnInit {
                     fileinfo.message = "Exceeds size limit of "+maxGB+" GB";
                     fileinfo.status = "REJECTED";
                     fail = true;
-                // } else {
-                //     this.suggestMessage = null;
                 }
                 this.fileInfos.push( fileinfo );
                 if( fail ) { this.fileBad += 1; }
@@ -81,26 +71,12 @@ export class MediaComponent implements OnInit {
         let fn: string = this.constructor.name + "#uploadFiles";  // tslint:disable-line:no-unused-variable
         console.log(fn + ": invoked");
 
-        // TODO: Workaround for video uploads, just use amazon. https://stackoverflow.com/questions/36010348/angular2-file-upload-for-amazon-s3-bucket
-
         console.log(fn + ": fileInfos = ", this.fileInfos );
         if( this.fileInfos.length > this.fileBad ) {
             this.submitService.fileInfos = this.fileInfos;
             this.errorMessage = null;
             this.succeedMessage = null;
             this.uploadFile(0);
-            // var request;
-            // let fileCount = this.fileInfos.length;
-            // for (let i = 0; i < fileCount; i++) {
-            // for( let fileinfo of this.fileInfos ) {
-
-                // var success;
-                // var percentComplete;
-                // var file = this.fileList[ i ];
-
-                // ... see uploadFile
-            // }
-
         } else {
             this.errorMessage = "Please select files to upload";
         }
@@ -122,9 +98,24 @@ export class MediaComponent implements OnInit {
                     fileinfo.message = "In Progress...";
                     fileinfo.status = "SENDING";
                     let request = new XMLHttpRequest();
-                    request.open("GET", this.endPoint.get_upload_link + fileinfo.file.name, true);
-                    console.log(fn + ": getting presigned link from ", this.endPoint.get_upload_link + fileinfo.file.name);
+
+                    console.log( fn + ": getting presigned link from ", this.endPoint.get_upload_link );
+                    request.open( "POST", this.endPoint.get_upload_link, true );
+
+                    request.setRequestHeader( "Content-Type", "application/json" );
+                    let outerTimeoutHandler = () => {
+                        console.error( fn+":/timeout: Timed Out!" );
+                        clearTimeout( outerTimeoutHandle );
+                        request.abort();
+                        fileinfo.message = "Error: Timed Out";
+                        fileinfo.status = "FAILED";
+                        fileinfo.progress = 0.0;
+                        this.uploadFile(i+1);
+                    };
+                    let outerTimeOut = 15000; //ms; 15 seconds
+                    let outerTimeoutHandle = setTimeout( outerTimeoutHandler, outerTimeOut );
                     request.onload = (oEvent) => {
+                        clearTimeout( outerTimeoutHandle );
                         console.log(fn + ": quoted presigned link = ", request.responseText);
                         let url = request.responseText;
                         if (request.responseText[ 0 ] == "\"" && request.responseText[ request.responseText.length - 1 ] == "\"") {
@@ -151,18 +142,12 @@ export class MediaComponent implements OnInit {
                             // console.log(fn + ":/onprogress: invoked with evt = ", evt);
                             clearTimeout( timeoutHandle );
                             timeoutHandle = setTimeout( timeoutHandler, timeOut );
-                            // if (evt.lengthComputable) {
-                            //     fileinfo.progress = evt.loaded / evt.total;
-                            //     console.log(fn + ":/onprogress: ", fileinfo.progress );
-                            // }
                             fileinfo.progress = evt.loaded / fileinfo.file.size;
                             this.ref.detectChanges();
                             console.log(fn + ":/onprogress: ", fileinfo.progress );
                         };
                         presigned_link.onload = (event) => {
                             console.log( fn+"/onload: ", presigned_link.statusText );
-                            // console.log( fn + "/onload: response from put", event );
-                            // console.log( fn + "/onload: response after put", presigned_link );
                             clearTimeout( timeoutHandle );
                             if( presigned_link.status === 200 ) {
                                 fileinfo.message = "Uploaded successfully!";
@@ -178,8 +163,6 @@ export class MediaComponent implements OnInit {
                         };
                         presigned_link.onerror = (err) => {
                             console.log( fn+"/onerror: ", presigned_link.statusText );
-                            // console.log( fn + "/onerror: response from put", err );
-                            console.log( fn + "/onerror: response after put", presigned_link );
                             clearTimeout( timeoutHandle );
                             let errmsg = ( presigned_link.statusText !== "" ) ? presigned_link.statusText : "unspecified error"
                             fileinfo.message = "Error: "+errmsg;
@@ -188,13 +171,11 @@ export class MediaComponent implements OnInit {
                             this.uploadFile(i+1);
                         }
                         presigned_link.open("PUT", url, true);
-                        // console.log(fn + ": presigned url opened");
                         presigned_link.setRequestHeader("Content-Type", fileinfo.file.type);
                         presigned_link.send(fileinfo.file);
                         console.log(fn + ": send has begun");
                     };
-                    // console.log( fn+": ", this.fileList[i] );
-                    request.send();
+                    request.send( JSON.stringify( { objectKey: fileinfo.file.name, contentType: fileinfo.file.type } ) );
                     break;
                 case "SENDING":
                     console.error( fn+": ABORT -- ALREADY SENDING!" );
