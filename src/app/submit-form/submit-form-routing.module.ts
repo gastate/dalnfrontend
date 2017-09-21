0f637ac6bf902a15c06f64c297d41d31b9f5c5d4
Wasfi Momen <wasfimomenwim@gmail.com>
2017-09-21 19:10:21 -0400

    new dev endpoint and allowed for submission to only url links?

--- src/app/submit-form/submit-form-routing.module.ts
+++ src/app/submit-form/submit-form-routing.module.ts
@@ -15,15 +15,15 @@ const submitFormRoutes: Routes = [
           path: '',
           component: SubmitFormComponent,
           children: [
-            {path: '', redirectTo: 'create', pathMatch: 'full'}
+            {path: '', redirectTo: 'create', pathMatch: 'full'},
             // { path: '', redirectTo: 'rights', pathMatch: 'full' },
-            // { path: 'rights', component: RightsComponent },
-            // { path: 'metadata', component: MetadataComponent },
-            // { path: 'description', component: DescriptionComponent },
-            // { path: 'media', component: MediaComponent },
-            // { path: 'license', component: LicenseComponent },
-            // { path: 'summary', component: SummaryComponent},
-            // { path: 'complete', component: CompleteComponent}
+            { path: 'rights', component: RightsComponent },
+            { path: 'metadata', component: MetadataComponent },
+            { path: 'description', component: DescriptionComponent },
+            { path: 'media', component: MediaComponent },
+            { path: 'license', component: LicenseComponent },
+            { path: 'summary', component: SummaryComponent},
+            { path: 'complete', component: CompleteComponent}
           ]
       },
 ];
