48b5075b3d173ed42da66cb205e852deb40e2882
CodeFluent <wasfimomenwim@gmail.com>
2017-10-12 12:14:34 -0400

    temp changing prod endpoint

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -32,9 +32,9 @@ const finalBucketName = "daln-prod";
 // Lambda Functions //
 //////////////////////
 
-const api_url = "https://svua18z1gd.execute-api.us-east-1.amazonaws.com/production";
+const api_url = "https://5odt95uhfg.execute-api.us-east-1.amazonaws.com/development";
 
-const search_api_url = "https://svua18z1gd.execute-api.us-east-1.amazonaws.com/production"; // using prod endpoint on both dev and prod.
+const search_api_url = "https://5odt95uhfg.execute-api.us-east-1.amazonaws.com/development"; // using prod endpoint on both dev and prod.
 
 const all_posts = api_url + "/posts/all";
 const create_post = api_url + "/posts/create";
