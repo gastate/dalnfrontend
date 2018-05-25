19829a2414d2ba3b5d37bdfa2ece0088644ae9ff
CodeFluent <wasfimomenwim@gmail.com>
2018-05-25 07:40:17 -0400

    not accurate at all

--- daln-metadata.py
+++ daln-metadata.py
@@ -1,12 +1,15 @@
+# Made with Python 2.7.15
+# requires pandas module
+
 import csv
 import os
 import glob
 import sys
-
-
+import pandas as pd
+from collections import Counter
 # ENV Variables
-path = "./Posts"
-num_files = 73  # please change this to the number of files in the directory
+path = "./Posts"  # please change this to the path where the csv files are
+num_files = 73   # please change this to the number of files in the directory
 
 
 # other variables for tracking
@@ -42,26 +45,31 @@ rows = []
 # reading csv fields
 for f in files:
     try:
-        fo = open(f, 'r')
-        csvreader = csv.reader(fo)
-        fields = csvreader.next()
+        fo = pd.read_csv(f)
 
-        # if you want header row (the json key values) uncomment
-        # print("\n\tField names are: " + ", ".join(field for field in fields))
+        # assetList row in csv
+        assetList = fo['assetList (L)']
+        types = ['"text"', '"audio"', '"audio/video"', '"file"']
+        asset_dict = {w: w.replace(' ', '_') for w in types}
 
-        # for field in fields:
-        #     fields.append(field)
+        # from https://stackoverflow.com/questions/30493364/sum-partial-string-key-word-matches-in-a-pandas-dataframe
+        corpus = ' '.join(str(v) for v in assetList.str.lower())
+        for w, w2 in asset_dict.items():
+            corpus = corpus.replace(w, w2)
 
-        for row in csvreader:
-            rows.append(row)
+        all_counts = Counter(corpus.split())
+        final_counts = {w: all_counts[w2] for w, w2 in asset_dict.items()}
 
         # increment total posts by the number of rows
         # found in the file
-        TOTAL_POSTS += csvreader.line_num
+        TOTAL_POSTS += assetList.count()
+        NUM_TEXT_ASSETS += final_counts['"text"']
+        NUM_TEXT_ASSETS += final_counts['"file"']
+        NUM_AUDIO_ASSETS += final_counts['"audio"']
+        NUM_VIDEO_ASSETS += final_counts['"audio/video"']
 
-        fo.close()
         num_processed_files += 1
-        processed_files.append(fo.name)
+        processed_files.append(f)
     except IOError as e:
         print (e)
 
@@ -69,6 +77,11 @@ for f in files:
 # Print out complete info
 print("\n--------METADATA INFO--------")
 print("\n\tTotal number of posts: %d" % (TOTAL_POSTS))
+print("\n\tEach type of asset found: ")
+print("\n\t\tText: " + str(NUM_TEXT_ASSETS))
+print("\n\t\tAudio: " + str(NUM_AUDIO_ASSETS))
+print("\n\t\tVideo: " + str(NUM_VIDEO_ASSETS))
+
 
 # uncomment for file I/O and other information on the program
 # sometimes returns an IO error, don't know why
@@ -77,6 +90,10 @@ print("\n\tTotal files processed: %d" % (num_processed_files))
 print("\n\t" + str(num_files - num_processed_files) +
       " files were not processed:\n")
 print(list(set(files) ^ set(processed_files)))
+
+# File write out
+# print(fo['assetList (L)'].to_csv(r'tweets.txt',
+#                                  header=None, index=None, sep=' ', mode='a'))
 
 
 # Future Functions
